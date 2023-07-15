dcfd256668190f4539879996d1a52ba1b4ec1c77
Saroj Padhan <40264980+saroj8455@users.noreply.github.com>
2023-07-16 00:39:16 +0530

    navbar and userservice added

--- angular-task/online-exam/src/app/app.module.ts
+++ angular-task/online-exam/src/app/app.module.ts
@@ -4,15 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { NavComponent } from './components/nav/nav.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSlideToggleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
