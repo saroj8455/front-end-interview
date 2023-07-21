6deaccfaec85417733666ef447e048fb3a596387
ocemblgr23 <131694566+ocemblgr23@users.noreply.github.com>
2023-07-21 18:06:15 +0530

    guard clauses and extract function js

--- nodejs-task/src/index.js
+++ nodejs-task/src/index.js
@@ -1,1 +1,55 @@
-console.log("Hello World App");
+const p = {
+  age:29
+}
+
+
+/**
+ *
+ * @param {*} person
+ * @returns
+ *
+ * Author : Web Dev Simplifed
+ * Guard clauses
+ */
+
+function canDrinkBetter(person) {
+  if(person?.age == null) {
+    console.log('You are not a person');
+    return; // end the execution
+  }
+
+
+  /**
+   *
+   * Guard clauses example
+   */
+
+  // if(person.age < 18 ) {
+  //   console.log('Nope 👎');
+  //   return;
+  // }
+
+  // if(person.age < 21) {
+  //   console.log('Not in the US 😂');
+  //   return
+  // }
+
+  // console.log('Yes you can, 👍');
+
+
+  //  extract functions
+  const result = canDrinkResponse(person.age)
+  console.log(result);
+
+
+}
+
+function canDrinkResponse(age) {
+  if(age<18) return "Nope 😂"
+  if(age<21) return "Not in the US 😊"
+  return "Yes ☺🙂"
+}
+
+
+
+canDrinkBetter(p)
