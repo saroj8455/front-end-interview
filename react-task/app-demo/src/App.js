2d06d9774b46bc9c54df9340d2ac037302f84821
Saroj Padhan <40264980+saroj8455@users.noreply.github.com>
2023-07-21 13:34:44 +0530

    material ui added

--- react-task/app-demo/src/App.js
+++ react-task/app-demo/src/App.js
@@ -1,11 +1,24 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Button from '@mui/material/Button';
 import './App.css';
+import { Rating, Typography } from '@mui/material';
 
 function App() {
+  const [value, setValue] = useState(2);
+
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className='App'>
+      <header className='App-header'>
         <h1>Hello World App</h1>
+        <Button variant='contained'>Hello World</Button>
+        <Typography component={'legend'}>Hello Typography</Typography>
+        <Rating
+          name='simple-controlled'
+          value={value}
+          onChange={( event,newValue) => {
+            setValue(newValue);
+          }}
+        />
       </header>
     </div>
   );
